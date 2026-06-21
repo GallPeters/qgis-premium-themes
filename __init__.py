b04c7704d2617e54491e246c6fe43a8f7922e042
Yossef Haim Kanter <p0026701@ness-tech.co.il>
2026-06-21 14:02:33 +0300

    Fix __ init__ relative path

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
 def classFactory(iface):
     """invoke plugin"""
-    from qgis_studio_themes.qgis_studio_themes import QgisStudioThemesPlugin, init_theme_paths  # pylint: disable=import-outside-toplevel
+    from .qgis_studio_themes import QgisStudioThemesPlugin, init_theme_paths  # pylint: disable=import-outside-toplevel
 
     init_theme_paths()
     return QgisStudioThemesPlugin(iface)
